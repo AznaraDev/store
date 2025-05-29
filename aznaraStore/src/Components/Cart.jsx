bb21b29f208ebfc673aa645048ad1cdda947e796
yanirc1981 <cerzurc@hotmail.com>
2025-05-29 15:21:52 -0300

    imagen carrito de ambos d y c

--- aznaraStore/src/Components/Cart.jsx
+++ aznaraStore/src/Components/Cart.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { incrementQuantity, removeFromCart, clearCart, decrementQuantity } from '../Redux/Actions/actions';
 import { Link, useNavigate } from 'react-router-dom';
 import { SlTrash, SlMinus, SlPlus } from "react-icons/sl";
-import backgroundImage from '../assets/img/banner.png'; // Cambia esta ruta según tu imagen
+import backgroundImage from '../assets/img/Dama/portadaLogin.png'; // Cambia esta ruta según tu imagen
 
 const Cart = () => {
   const dispatch = useDispatch();
