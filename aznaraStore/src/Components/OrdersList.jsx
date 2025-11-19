c89b11ab9814055e5fbffd8a2adb831bbe89cf56
mlobeto <mercedeslobeto@gmail.com>
2025-11-19 20:59:20 -0300

    order list

--- aznaraStore/src/Components/OrdersList.jsx
+++ aznaraStore/src/Components/OrdersList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllOrders, updateOrderState } from '../Redux/Actions/actions';
 import Swal from 'sweetalert2';
