696fb5640874d4a7675c9a245c9ef9f99d2af207
mlobeto <mercedeslobeto@gmail.com>
2025-11-20 18:28:41 -0300

    sin commmenn

--- AznaraBack/src/data/models/OrderDetail.js
+++ AznaraBack/src/data/models/OrderDetail.js
@@ -69,7 +69,7 @@ module.exports = (sequelize) => {
         type: DataTypes.ENUM('Pago en local', 'Pago contra entrega', 'Pago online (Wompi)'),
         allowNull: false,
         defaultValue: 'Pago en local',
-        comment: 'Método de pago seleccionado por el cliente'
+        
       },
       integritySignature: {
         type: DataTypes.STRING,
