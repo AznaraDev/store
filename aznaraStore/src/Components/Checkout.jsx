f3878fdb1f1387853b2cbd21e59868bfb0d4a945
mlobeto <mercedeslobeto@gmail.com>
2026-02-04 14:30:09 -0300

    <Retira en local

--- aznaraStore/src/Components/Checkout.jsx
+++ aznaraStore/src/Components/Checkout.jsx
@@ -214,8 +214,9 @@ const Checkout = () => {
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             required
           >
+             <option value="Envio a domicilio">Envio a domicilio</option>
             <option value="Retira en local">Retira en local</option>
-            <option value="Envio a domicilio">Envio a domicilio</option>
+           
           </select>
         </div>
         
